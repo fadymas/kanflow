50b3f377a9da7de9c97b41b422c44c353a36b681
Fady Mahrous <fadymahros373@gmail.com>
2026-05-21 19:54:01 +0300

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,10 @@
 import type { MetadataRoute } from 'next'
+export const dynamic = 'force-dynamic'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_URL!
+  const baseUrl = process.env.NEXT_PUBLIC_URL
+    ? process.env.NEXT_PUBLIC_URL
+    : 'https://kanflow-two.vercel.app'
 
   return [
     {
